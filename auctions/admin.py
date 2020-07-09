02e891412672b2b168c46e54fa672dda056c3fc5
ArturKL <arturkalimullin10@gmail.com>
2020-07-09 19:14:34 +0300

    Created models

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import User
+from .models import *
 
 admin.site.register(User)
+admin.site.register(ListingCategory)
+admin.site.register(Listing)
+admin.site.register(Bid)
+admin.site.register(Comment)
--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,30 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class ListingCategory(models.Model):
+    title = models.CharField(max_length=64)
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=1000)
+    starting_bid = models.DecimalField(max_digits=14, decimal_places=2)
+    image_url = models.CharField(max_length=300, blank=True)
+    catergory = models.ForeignKey(ListingCategory, on_delete=models.SET_NULL, related_name='category', null=True)
+    active = models.BooleanField(default=True)
+    subscribers = models.ManyToManyField(User, related_name='subscriptions', blank=True)
+
+
+class Bid(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bids')
+    listing_id = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='bids')
+    value = models.DecimalField(max_digits=14, decimal_places=2)
+
+
+class Comment(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    listing_id = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='comments')
+    content = models.CharField(max_length=1000)
+
